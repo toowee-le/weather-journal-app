2264eda428d0f853ac7e003d492f031e7209ec33
Tuyet Le <toowee.le@gmail.com>
2020-06-04 11:23:40 +0100

    set up a node and express environment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+/* Middleware */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialise the main project folder
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server is running on localhost: ${port}`));
