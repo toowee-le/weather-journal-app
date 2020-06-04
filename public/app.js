f38165a249a510b72b0d1b0faade189d7a2fcefa
Tuyet Le <toowee.le@gmail.com>
2020-06-04 14:35:44 +0100

    add alert if input fields are empty

--- public/app.js
+++ public/app.js
@@ -3,21 +3,43 @@ const apiKey = '&appid=2d11d5d06fde9eec74cf187fb75aaca2';
 
 document.getElementById('generate').addEventListener('click', performAction);
 
-function performAction() {
-    console.log('clicked');
-    const city = document.getElementById('city').value;
-    const journalEntry = document.getElementById('feelings').value;
-    getWeather(baseURL, city, apiKey);
-    console.log(journalEntry);
-}
-
+function performAction(e) {
+    e.preventDefault();
+    const form = document.forms["journalForm"]["feelings"].value;
+    if (form == "") {
+        alert("Please describe your mood today.");
+        return false;
+    } else {
+        const city = document.getElementById('city').value;
+        const journalEntry = document.getElementById('feelings').value;
+        getWeather(baseURL, city, apiKey)
+        .then(data => console.log(data));
+    };
+};
+ 
 const getWeather = async (baseURL, city, key) => {
-    let response = await fetch(baseURL+city+key);
-    let data = await response.json();
+    const response = await fetch(baseURL+city+key);
+    const data = await response.json();
     try {
-        console.log(data);
         return data;
     } catch (error) {
         console.log("Error", error);
+    };
+};
+
+const postData = async (url = '', data = {}) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    try {
+        const getData = await response.json();
+        return getData;
+    } catch (error) {
+        console.log("Error:", error);
     };
 };
